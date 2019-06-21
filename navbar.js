9b15add862e10ec7f967488a4d14d2c2737a830c
junqiwu02 <junqiwu02@gmail.com>
2019-06-21 18:21:34 -0400

    removed seal on mobile devices

--- navbar.js
+++ navbar.js
@@ -1,14 +1,23 @@
-// When the user scrolls down 20px from the top of the document, slide down the navbar
-// When the user scrolls to the top of the page, slide up the navbar (50px out of the top view)
-window.onscroll = function() {scrollFunction()};
+let isMobile = typeof window.orientation !== "undefined" || navigator.userAgent.indexOf('IEMobile') !== -1;
+
+window.onscroll = function () {
+    if (!isMobile) {
+        scrollFunction();
+    }
+};
+
+if (isMobile) {
+    document.getElementById("seal").remove();
+    document.getElementsByClassName("spacer")[0].style.padding = "20px 0";
+}
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("seal").style.height = "0";
-    document.getElementById("seal").style.margin = "10px";
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("seal").style.height = "0";
+        document.getElementById("seal").style.margin = "10px";
 
-  } else {
-    document.getElementById("seal").style.height = "90px";
-    document.getElementById("seal").style.margin = "30px";
-  }
+    } else {
+        document.getElementById("seal").style.height = "90px";
+        document.getElementById("seal").style.margin = "30px";
+    }
 }
