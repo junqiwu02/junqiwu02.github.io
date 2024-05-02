14590735046af5fd8215c0ed17f3b1a949109473
Junqi <junqiwu02@gmail.com>
2024-05-02 18:09:14 -0400

    vite setup

--- search/search.js
+++
@@ -1,5 +0,0 @@
-function search() {
-    let query = document.getElementById("query").value;
-    window.open('https://www.google.com/search?q=' + query + ' site:junqiwu02.github.io');
-    return false;
-}
